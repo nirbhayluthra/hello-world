52f51540d64c1a0c1a656fe66f17be7baff83495
nirbhayluthra <nirbhayluthra@gmail.com>
2019-08-31 18:06:30 +0530

    release

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Hello World
+// anything
+
+#include<iostream>
+
+ using namespace std;
+
+ int main()
+ {
+     cout << "hellllloooo wooooorlllllllld" << flush;
+          cout << "hellllloooo wooooorlllllllld" << endl;
+    cout <<"heeee    "  << "jdkdkd    " << endl;
+     return 0;
+
+     }
